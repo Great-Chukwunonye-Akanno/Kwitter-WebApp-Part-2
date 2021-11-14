be8eaf6159f8ab601b0a23989b86bdae291e13d5
Great-Chukwunonye-Akanno <83137882+Great-Chukwunonye-Akanno@users.noreply.github.com>
2021-11-14 13:51:43 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBmZYPgLF02IzNN-qRtz49M3WiCfrAfWHY",
+    authDomain: "kwitter-webapp-3b6ad.firebaseapp.com",
+    databaseURL: "https://kwitter-webapp-3b6ad-default-rtdb.firebaseio.com",
+    projectId: "kwitter-webapp-3b6ad",
+    storageBucket: "kwitter-webapp-3b6ad.appspot.com",
+    messagingSenderId: "767649661612",
+    appId: "1:767649661612:web:8e5ef7171f59c1a60c9509"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
